4473038afdf419d9600c463345bdd1bc14fbac13
wade <wade_mansell@hotmail.com>
2022-11-17 23:27:00 +1100

    whitenoise compressedmanifest setting

--- core/settings.py
+++ core/settings.py
@@ -176,6 +176,8 @@ INTERNAL_IPS = [
     '127.0.0.1'
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
